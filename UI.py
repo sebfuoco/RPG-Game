071a2255cedfe603d86c68458d9fd6eb618b11b8
sebfuoco <58778353+sebfuoco@users.noreply.github.com>
2020-07-11 21:28:24 +0100

    Dynamic UI + Colour
    Further improvements to the UI has been made (show current equipment and stats) as well as colour being added to the map.

--- UI.py
+++ UI.py
@@ -1,48 +1,78 @@
+import curses
+
 from Map import *
 class mainUI:
 	def worldUI(self, player):
-		i = 0
-		pos = 0
-		loopUI(i, pos, Maps.currentMap, self)
-		mainUI.mapNameUI(self)
+		loopMap(Maps.currentMap, self)
+		size = mainUI.mapNameUI(self)
 		# CHARACTER
-		mainUI.characterUI(self, player)
+		size = mainUI.characterUI(self, player)
 		# INVENTORY
-		mainUI.charInventoryUI(self, player)
-		# COMMANDS
-		mainUI.commandsUI(self)
+		size = mainUI.charInventoryUI(self, player)
+		# BATTLELOG
+		size = mainUI.battleLogUI(self)
+		return size
 
 	def characterUI(self, player):
-		quickCharacter =   [["+-----------------------------+"],
-							[f"| {player.stats['CLASS']}|"],
-							[f"| HP: {player.stats['HP']} / {player.currentStats['MaxHP']}                 |"],
-							[f"| MP:   {player.stats['MP']} / {player.currentStats['MaxMP']}               |"],
-							["+-----------------------------+"]]
-		i = 6
-		pos = 33
-		loopUI(i, pos, quickCharacter, self)
+		try:
+			quickCharacter =   [["+-----------------------------+"],
+								["|                             |"],
+								["| HP:                         |"],
+								["| MP:                         |"],
+								["+-----------------------------+"]]
+			i = 0
+			pos = 33
+			loopUI(i, pos, quickCharacter, self)
+			self.addstr(1, 35, player.stats["CLASS"] + "          LEVEL: " + str(player.currentStats["LEVEL"]))
+			self.addstr(2, 39, str(player.stats["HP"]) + " / " + str(player.currentStats["MaxHP"]))
+			self.addstr(3, 39, str(player.stats["MP"]) + " / " + str(player.currentStats["MaxMP"]))
+			size = True
+		except curses.error:
+			print("TERMINAL SIZE TOO SMALL, PLEASE RESIZE!")
+			size = False
+		return size
 
 	def charInventoryUI(self, player):
-		charInv =  [["+-----------------------------+"],
-					[f"| STR: {player.stats['STR']}   DEF: {player.stats['DEF']}             |"],
-					["| HEAD: EMPTY                 |"],
-					["| CHEST: LEATHER-BREASTPLATE  |"],
-					["| LEFT-ARM: EMPTY             |"],
-					["| RIGHT-ARM: WOODEN-SWORD     |"],
-					["+-----------------------------+"]]
-		i = 0
-		pos = 33
-		loopUI(i, pos, charInv, self)
+		try:
+			charInv =  [["+-----------------------------+"],
+						["|                             |"],
+						["|                             |"],
+						["|                             |"],
+						["|                             |"],
+						["|                             |"],
+						["+-----------------------------+"]]
+			i = 4
+			pos = 33
+			loopUI(i, pos, charInv, self)
+			self.addstr(5, 35, "STR: " + str(player.stats["STR"]) + " DEF: " + str(player.stats["DEF"]))
+			self.addstr(6, 35, "HEAD: " + player.equipped["HEAD"]["name"])
+			self.addstr(7, 35, "CHEST: " + player.equipped["CHEST"]["name"])
+			self.addstr(8, 35, "LEFT-HAND: " + player.equipped["LEFT-HAND"]["name"])
+			self.addstr(9, 35, "RIGHT-HAND: " + player.equipped["RIGHT-HAND"]["name"])
+			size = True
+		except curses.error:
+			print("TERMINAL SIZE TOO SMALL, PLEASE RESIZE!")
+			size = False
+		return size
 
-	def commandsUI(self):
-		commands = [["+-----------------------------+"],
-					["| INVENTORY   ATTACK   TALK   |"],
-					["|                             |"],
-					["|                             |"],
-					["+-----------------------------+"]]
-		i = 10
-		pos = 33
-		loopUI(i, pos, commands, self)
+	def battleLogUI(self):
+		try:
+			commands = [["+-----------------------------+"],
+						["| BATTLE LOG                  |"],
+						["|                             |"],
+						["|                             |"],
+						["|                             |"],
+						["|                             |"],
+						["|                             |"],
+						["+-----------------------------+"]]
+			i = 10
+			pos = 33
+			loopUI(i, pos, commands, self)
+			size = True
+		except curses.error:
+			print("TERMINAL SIZE TOO SMALL, PLEASE RESIZE!")
+			size = False
+		return size
 
 	def mapNameUI(self):
 		mapName =  [["+------------------------------+"],
@@ -50,8 +80,14 @@ class mainUI:
 					["+------------------------------+"]]
 		i = 15
 		pos = 0
-		loopUI(i, pos, mapName, self)
-		self.addstr(16, 2, Maps.currentMapData["name"])
+		try:
+			loopUI(i, pos, mapName, self)
+			self.addstr(16, 2, Maps.currentMapData["name"])
+			size = True
+		except curses.error:
+			print("TERMINAL SIZE TOO SMALL, PLEASE RESIZE!")
+			size = False
+		return size
 
 	def inventory(self):
 		commands = [["+------------------------------+"],
@@ -62,6 +98,35 @@ class mainUI:
 		i = 0
 		pos = 32
 		loopUI(i, pos, commands, self)
+
+def loopMap(item, screen):
+	try:
+		col, row = (0, 0)
+		while col < len(item):
+			for i in enumerate(item[0][0]):
+				if item[col][0][row] in "X":
+					screen.addstr(col, row, item[col][0][row], curses.color_pair(1))
+				elif item[col][0][row] in "*":
+					screen.addstr(col, row, item[col][0][row], curses.color_pair(11))
+				elif item[col][0][row] in "M":
+					screen.addstr(col, row, item[col][0][row], curses.color_pair(12))
+				elif item[col][0][row] in ("˄", "Q"):
+					screen.addstr(col, row, item[col][0][row], curses.color_pair(7))
+				elif item[col][0][row] in "C":
+					screen.addstr(col, row, item[col][0][row], curses.color_pair(7))
+				elif item[col][0][row] in ("|", "-"):
+					screen.addstr(col, row, item[col][0][row], curses.color_pair(9))
+				elif item[col][0][row] in ("=", "ǁ", "+",".", "#"): # Obstructions not coloured
+					screen.addstr(col, row, item[col][0][row])
+				else:
+					screen.addstr(col, row, item[col][0][row], curses.color_pair(5))
+				row += 1
+			row = 0
+			col += 1
+	except curses.error:
+		print("TERMINAL SIZE TOO SMALL, PLEASE RESIZE!")
+		size = False
+		return size
 
 def loopUI(i, pos, item, screen):
 	for r in item:
--- main.py
+++ main.py
@@ -1,58 +1,77 @@
 import curses
 from curses import wrapper
 from UI import *
+
+
 class Equipment:
-	# Defense Equipment (Name, Price, EquipLocation, EquipClass, Stats)
-	# Stats: (Attack, AccuracyBoost, Defense, Magic Defense, EvasionBoost, MagicBoost)
+	# Defense Equipment
 	# All Class Equipment
-	Empty = {"Empty", 0, 0, "all", (0, 0, 0, 0, 0, 0)}
-	Clothes = {"Clothes", 1, 2, "all", (0, 0, 1, 0, 0, 0)}
-
+	Empty = {"name": "EMPTY", "price": 0, "equipLocation": "ALL", "equipClass": "ALL", "stats": (0, 0)}
+	Clothes = {"name": "CLOTHES", "price": 1, "equipLocation": "CHEST", "equipClass": "ALL", "stats": (0, 1)}
 	# Warrior Equipment
-	LeatherBreastplate = {"Leather Breastplate", 0, 2, "warrior", (0, 0, 2, 0, 0, 0)}
-
+	LeatherBreastplate = {"name": "LEATHER-BREASTPLATE", "price": 2, "equipLocation": "CHEST", "equipClass": "WARRIOR",
+						  "stats": (0, 2)}
 	# Mage Equipment
-	Robe = {"Robe", 1, 2, "mage", (0, 0, 0, 5, 0, 1)}
-
+	Robe = {"name": "ROBE", "price": 1, "equipLocation": "CHEST", "equipClass": "MAGE", "stats": (0, 2)}
 	# Thief Equipment
-	Bandana = {"Bandana", 0, 1, "thief", (0, 0, 1, 0, 2, 0)}
-
+	Bandana = {"name": "BANDANA", "price": 1, "equipLocation": "HEAD", "equipClass": "THIEF", "stats": (0, 1)}
 	# Offense Equipment
 	# All Class Equipment
-	Knife = {"Knife", 1, "all", (2, 0, 0, 0, 0, 0)}
-
+	Knife = {"name": "KNIFE", "price": 3, "equipLocation": "HAND", "equipClass": "ALL", "stats": (2, 0)}
 	# Warrior Equipment
-	WoodenSword = {"Wooden Sword", 5, ("warrior", "thief"), (1, 2, 0, 0, 0, 0)}
-
+	WoodenSword = {"name": "WOODEN-SWORD", "price": 1, "equipLocation": "HAND", "equipClass": ("WARRIOR", "THIEF"),
+				   "stats": (1, 0)}
 	# Mage Equipment
-	WoodenWand = {"Wooden Wand", 1, "mage", (1, 0, 0, 0, 0, 0)}
-
+	WoodenWand = {"name": "WOODEN WAND", "price": 1, "equipLocation": "HAND", "equipClass": "MAGE", "stats": (1, 0)}
 	# Thief Equipment
-	Dagger = {"Dagger", 10, "all", (5, 0, 0, 0, 0, 0)}
+	Dagger = {"name": "DAGGER", "price": 10, "equipLocation": "HAND", "equipClass": "ALL", "stats": (5, 0)}
+
 
 class Classes:
 	# Warrior Stats + Factors
 	class Warrior:
 		stats = {"CLASS": "WARRIOR", "HP": 20, "MP": 0, "STR": 5, "DEF": 1}
 		nextLevel = {"HPIncrease": 5, "MPIncrease": 0, "STRIncrease": 1}
+		equipped = {"HEAD": Equipment.Empty, "CHEST": Equipment.Clothes, "LEFT-HAND": Equipment.Empty,
+					"RIGHT-HAND": Equipment.WoodenSword}
 
 	# Mage Stats + Factors
 	class Mage:
-		stats = {"CLASS": "MAGE","HP": 20, "MP": 20, "STR": 1, "DEF": 0}
+		stats = {"CLASS": "MAGE", "HP": 20, "MP": 20, "STR": 1, "DEF": 0}
 		nextLevel = {"HPIncrease": 2, "MPIncrease": 5, "STRIncrease": 1}
+		equipped = {"HEAD": Equipment.Empty, "CHEST": Equipment.Clothes, "LEFT-HAND": Equipment.Empty,
+					"RIGHT-HAND": Equipment.WoodenWand}
 
 	# Thief Stats + Factors
 	class Thief:
 		stats = {"CLASS": "THIEF", "HP": 20, "MP": 0, "STR": 3, "DEF": 0}
 		nextLevel = {"HPIncrease": 3, "MPIncrease": 0, "STRIncrease": 1}
+		equipped = {"HEAD": Equipment.Bandana, "CHEST": Equipment.Clothes, "LEFT-HAND": Equipment.Empty,
+					"RIGHT-HAND": Equipment.WoodenSword}
+
 
 class Player(object):
-	def __init__(self,stats, nextLevel):
-		Level = 1
+	def __init__(self, stats, nextLevel, equipped):
 		XP = 20
-		self.currentStats = {"MaxHP": stats["HP"], "MaxMP": stats["MP"]}
-		self.stats = {"CLASS": stats["CLASS"], "HP": stats["HP"], "MP": stats["MP"], "STR": stats["STR"], "DEF": stats["DEF"]}
-		self.nextLevel = {"HPIncrease": nextLevel["HPIncrease"], "MPIncrease": nextLevel["MPIncrease"], "STRIncrease": nextLevel["STRIncrease"]}
+		self.currentStats = {"LEVEL": 1, "MaxHP": stats["HP"], "MaxMP": stats["MP"]}
+		self.stats = {"CLASS": stats["CLASS"], "HP": stats["HP"], "MP": stats["MP"], "STR": stats["STR"],
+					  "DEF": stats["DEF"]}
+		self.nextLevel = {"HPIncrease": nextLevel["HPIncrease"], "MPIncrease": nextLevel["MPIncrease"],
+						  "STRIncrease": nextLevel["STRIncrease"]}
+		self.equipped = {"HEAD": equipped["HEAD"], "CHEST": equipped["CHEST"], "LEFT-HAND": equipped["LEFT-HAND"],
+						 "RIGHT-HAND": equipped["RIGHT-HAND"]}
+
+
+class mob(object):
+	def __init__(self, stats):
+		self.stats = {"name": stats["name"], "HP": stats["HP"], "STR": stats["STR"], "DEF": stats["DEF"],
+					  "XP": stats["XP"]}
+
+
+class mobList:
+	goblin = mob({"name": "GOBLIN", "HP": 10, "STR": 2, "DEF": 1, "XP": 5})
+	rat = mob({"name": "RAT", "HP": 8, "STR": 4, "DEF": 0, "XP": 8})
+
 
 def my_raw_input(screen, r, c, prompt_string, charlength):
 	curses.echo()
@@ -61,41 +80,54 @@ def my_raw_input(screen, r, c, prompt_string, charlength):
 	input = screen.getstr(r + 1, c, charlength)
 	return input
 
+
 def main(main):
 	yCoord = 11
 	xCoord = 10
+	size = True
 	spawnLocation = 0
 	# print(Equipment.Empty)
 	screen = curses.initscr()
 	curses.start_color()
+	curses.use_default_colors()
+	for i in range(0, 15):
+		curses.init_pair(i + 1, i, 0)
+	# for i in range(0, 15):
+	# screen.addstr(str(i), curses.color_pair(i))
+	screen.refresh()
+	# screen.getstr()
 	while True:
 		screen.clear()
-		screen.addstr(0,0, f"1. Warrior | HP: {str(Classes.Warrior.stats['HP'])} MP: {str(Classes.Warrior.stats['MP'])} STR: {str(Classes.Warrior.stats['STR'])} DEF: {str(Classes.Warrior.stats['DEF'])}")
-		screen.addstr(1,0, f"2. Mage    | HP: {str(Classes.Mage.stats['HP'])} MP: {str(Classes.Mage.stats['MP'])} STR: {str(Classes.Mage.stats['STR'])} DEF: {str(Classes.Mage.stats['DEF'])}")
-		screen.addstr(2,0, f"3. Thief   | HP: {str(Classes.Thief.stats['HP'])} MP: {str(Classes.Thief.stats['MP'])} STR: {str(Classes.Thief.stats['STR'])} DEF: {str(Classes.Thief.stats['DEF'])}")
+		screen.addstr(0, 0,
+					  f"1. Warrior | HP: {str(Classes.Warrior.stats['HP'])} MP: {str(Classes.Warrior.stats['MP'])} STR: {str(Classes.Warrior.stats['STR'])} DEF: {str(Classes.Warrior.stats['DEF'])}")
+		screen.addstr(1, 0,
+					  f"2. Mage    | HP: {str(Classes.Mage.stats['HP'])} MP: {str(Classes.Mage.stats['MP'])} STR: {str(Classes.Mage.stats['STR'])} DEF: {str(Classes.Mage.stats['DEF'])}")
+		screen.addstr(2, 0,
+					  f"3. Thief   | HP: {str(Classes.Thief.stats['HP'])} MP: {str(Classes.Thief.stats['MP'])} STR: {str(Classes.Thief.stats['STR'])} DEF: {str(Classes.Thief.stats['DEF'])}")
 		choice = my_raw_input(screen, 3, 0, "Type in a number below:", 1).decode("utf-8")
 		screen.refresh()
 		if choice == "1":
-			player = Player(Classes.Warrior.stats, Classes.Warrior.nextLevel)
+			player = Player(Classes.Warrior.stats, Classes.Warrior.nextLevel, Classes.Warrior.equipped)
 			break
 		elif choice == "2":
-			player = Player(Classes.Mage.stats, Classes.Mage.nextLevel)
+			player = Player(Classes.Mage.stats, Classes.Mage.nextLevel, Classes.Mage.equipped)
 			break
 		elif choice == "3":
-			player = Player(Classes.Thief.stats, Classes.Thief.nextLevel)
+			player = Player(Classes.Thief.stats, Classes.Thief.nextLevel, Classes.Thief.equipped)
 			break
 		else:
 			screen.clear()
 			screen.addstr(0, 0, "Not an option")
 			screen.refresh()
-			curses.napms(2000)
-	mainUI.worldUI(screen, player)
+			curses.napms(500)
+	screen.clear()
+	size = mainUI.worldUI(screen, player)
 	movePlayer(screen, yCoord, xCoord)
 	curses.noecho()
 	curses.curs_set(0)
-	#findPos(yCoord, xCoord)
-	#choice = my_raw_input(screen, 12, 35, "Action?", 10).upper().decode("utf-8")
-	while True:
+	# findPos(yCoord, xCoord)
+	# choice = my_raw_input(screen, 12, 35, "Action?", 10).upper().decode("utf-8")
+	while size == True:
 		key = screen.getch()
 		if key == curses.KEY_HOME:
 			break
@@ -106,7 +138,7 @@ def main(main):
 					spawnLocation = respawnData(yCoord, xCoord, spawnLocation)
 					yCoord, xCoord = loadNextMap(yCoord, xCoord, spawnLocation)
 					screen.clear()
-					mainUI.worldUI(screen, player)
+					size = mainUI.worldUI(screen, player)
 				else:
 					currentPosition(screen, yCoord, xCoord)
 					yCoord -= 1
@@ -118,7 +150,7 @@ def main(main):
 					spawnLocation = respawnData(yCoord, xCoord, spawnLocation)
 					yCoord, xCoord = loadNextMap(yCoord, xCoord, spawnLocation)
 					screen.clear()
-					mainUI.worldUI(screen, player)
+					size = mainUI.worldUI(screen, player)
 				else:
 					currentPosition(screen, yCoord, xCoord)
 					yCoord += 1
@@ -130,7 +162,7 @@ def main(main):
 					spawnLocation = respawnData(yCoord, xCoord, spawnLocation)
 					yCoord, xCoord = loadNextMap(yCoord, xCoord, spawnLocation)
 					screen.clear()
-					mainUI.worldUI(screen, player)
+					size = mainUI.worldUI(screen, player)
 				else:
 					currentPosition(screen, yCoord, xCoord)
 					xCoord -= 1
@@ -142,7 +174,7 @@ def main(main):
 					spawnLocation = respawnData(yCoord, xCoord, spawnLocation)
 					yCoord, xCoord = loadNextMap(yCoord, xCoord, spawnLocation)
 					screen.clear()
-					mainUI.worldUI(screen, player)
+					size = mainUI.worldUI(screen, player)
 				else:
 					currentPosition(screen, yCoord, xCoord)
 					xCoord += 1
@@ -150,6 +182,7 @@ def main(main):
 
 		screen.refresh()
 	curses.endwin()
+
 
 if __name__ == "__main__":
 	wrapper(main)
